9487179e837471a43bb118a295ed0c42705536b5
zachc <childers.zd@gmail.com>
2021-01-25 18:46:19 -0500

    Options for Webscraping different things Presently:
    - Weather
    - Cat
    - Whatever tf you want
    - NASDAQ stocks stuff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,9 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import javax.print.Doc;
 import javax.swing.*;
-import java.awt.*;
 import java.util.Scanner;
 import static java.lang.System.*;
 
@@ -13,26 +14,74 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         int loop = 0;
-        String loopRes;
+        int userChoice;
+        String loopRes, searchParam, usrOpt;
         Scanner input = new Scanner(in);
         try {
             do {
-                Document doc = Jsoup.connect("http://theoldreader.com/kittens/600/400/js").get();
-                // get the page title
-                String title = doc.title();
-                out.println("title: " + title);
+                userChoice = menu();
+                if(userChoice == 1){
+                    Document doc = Jsoup.connect("http://theoldreader.com/kittens/600/400/js").get();
+                    Elements links = doc.select("a[href]");
+                    for (Element link : links) {
+                        // get the value from the href attribute
+                        out.println("\nlink: " + link.attr("href"));
+                    }
+                } else {
+                    if(userChoice==2){
+                        out.print("what are we looking for: ");
+                        searchParam=input.next();
+                        Document doc = Jsoup.connect("https://www.google.com/search?q="+searchParam).get();
+                        // get the page title
+                        String title = doc.title();
+                        out.println("title: " + title);
 
-                // get all links in page
-                Elements links = doc.select("a[href]");
-                for (Element link : links) {
-                    // get the value from the href attribute
-                    out.println("\nlink: " + link.attr("href"));
-                    out.println("text: " + link.text());
+                        // get all links in page
+                        Elements links = doc.select("a[href]");
+                        for (Element link : links) {
+                            // get the value from the href attribute
+                            out.println("\nlink: " + link.attr("href"));
+                            out.println("text: " + link.text());
+                        }
+
+                    }else{
+                        if(userChoice==3){
+                            Document doc = Jsoup.connect("https://weather.com/weather/tenday/l/Spartanburg+SC?canonicalCityId=8b77caf9f6ae0ffbed40f5745635a09fafbf137ec2e9fcd56ff70f0d077f259d").get();
+                            // get the page title
+                            String title = doc.title();
+                            out.println("title: " + title);
+
+                            // get all links in page
+                            Elements links = doc.select("a[href]");
+                            for (Element link : links) {
+                                // get the value from the href attribute
+                                out.println("\nlink: " + link.attr("href"));
+                                out.println("text: " + link.text());
+                            }
+
+
+                        } else{
+                            if (userChoice==4){
+                                Document doc =Jsoup.connect("https://www.nasdaq.com/market-activity/stocks").get();
+                                String title = doc.title();
+                                out.println("title: " + title);
+
+                                // get all links in page
+                                Elements links = doc.select("a[href]");
+                                for (Element link : links) {
+                                    // get the value from the href attribute
+                                    out.println("\nlink: " + link.attr("href"));
+                                    out.println("text: " + link.text());
+                                }
+                            }
+                        }
+                    }
                 }
                 out.print("Wanna see me do it again? (Y/N): ");
                 loopRes = input.next();
                 if(loopRes.equalsIgnoreCase("n")) {
-                    if (JOptionPane.showConfirmDialog(null, "Are you sure?", "WARNING",
+                    if (JOptionPane.showConfirmDialog(null,
+                            "Are you sure?", "WARNING",
                             JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
                         System.exit(0);
                     } else {
@@ -41,7 +90,7 @@ public class Main {
 
                 }
                 else{
-                    out.print("Continuing...");
+                    out.print("Continuing...\n");
                     continue;
                 }
 
@@ -62,4 +111,24 @@ public class Main {
         JOptionPane.showMessageDialog(null, s, "Oops..." +
                 s1, JOptionPane.INFORMATION_MESSAGE);
     }
+
+    public static int menu() {
+
+        int selection;
+        Scanner input = new Scanner(System.in);
+
+        /***************************************************/
+
+        out.println("What are we scraping today?");
+        out.println("-------------------------\n");
+        out.println("1 - Cat Picture");
+        out.println("2 - Scrape the web");
+        out.println("3 - Weather Scrape");
+        out.println("4 - NASDAQ Scrape (Market Activity, Stocks)");
+        out.print("Enter choice: ");
+        selection = input.nextInt();
+        return selection;
+    }
+
+
 }
